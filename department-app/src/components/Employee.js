8a2fa6b2115a4681f78e1a6423e041b4da2069d9
mightyathasy <akos.toth953@gmail.com>
2021-10-24 18:02:32 +0200

    initial commit

---
+++ department-app/src/components/Employee.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Employee extends React.Component{
+    
+    render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                This is Employee page.
+            </div>
+        )
+    }
+}
+
+export default Employee;
